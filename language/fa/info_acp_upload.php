b3d4ce20ce014f8b2c277cf61dddd9347d444653
Meisam Nobari <meis2m@yahoo.com>
2014-09-28 11:20:59 +0330

    add info_acp_upload.php

---
+++ language/fa/info_acp_upload.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package Upload Extensions
+* @copyright (c) 2014 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+if (!defined('IN_PHPBB'))
+{
+exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+$lang = array();
+}
+$lang = array_merge($lang, array(
+'ACP_UPLOAD_EXT_TITLE'	=> 'آپلود افزونه',
+'ACP_UPLOAD_EXT_CONFIG_TITLE'	=> 'آپلود افزونه',
+));
