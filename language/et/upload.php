f474a79e9802d68576d3699b6489e97e1bb2eb92
phpBBeesti <info@phpbbeesti.com>
2014-11-03 20:00:19 +0200

    Update upload.php

--- language/et/upload.php
+++ language/et/upload.php
@@ -18,24 +18,24 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_UPLOAD_EXT_TITLE'				=> 'Lae üles laiendusi',
-	'ACP_UPLOAD_EXT_CONFIG_TITLE'		=> 'Lae üles laiendusi',
-	'ACP_UPLOAD_EXT_TITLE_EXPLAIN'		=> 'Lae üles laiendusi lubab sul üles laadida laiendusite zip faile või kustutada laienduste kauste serverist.<br />Selle laiendusega sa saad paigaldada/uuendada/kustutada laiendusi ilma, et peaksid kasutama FTP\'d. Kui aga üles laetud laiendus juba eksisteerib, siis seda uuendatakse üles laetud failidega.',
-	'UPLOAD'							=> 'Lae üles',
+	'ACP_UPLOAD_EXT_TITLE'				=> 'Laadi laiendus üles',
+	'ACP_UPLOAD_EXT_CONFIG_TITLE'		=> 'Laadi laiendus üles',
+	'ACP_UPLOAD_EXT_TITLE_EXPLAIN'		=> 'Laadi laiendus üles lubab sul üles laadida laienduste zip faile või kustutada nende kauste serverist.<br />Antud laiendusega sa saad paigaldada/uuendada/kustutada laiendusi ilma, et peaksid kasutama FTP\'d. Kui aga üles laetud laiendus juba eksisteerib, siis seda uuendatakse üles laetud failidega.',
+	'UPLOAD'							=> 'Laadi üles',
 	'BROWSE'							=> 'Sirvi...',
-	'EXTENSION_UPLOAD'					=> 'Lae üles laiendus',
-	'EXTENSION_UPLOAD_EXPLAIN'			=> 'Siin saad üles laadida kokku pakitud .zip faile. “Lae üles laiendusi” pakib selle ise lahti, ning seadistab selle sulle paigaldamiseks valmis.<br />Vali fail arvutist või kirjuta veebilehe aadress all olevale väljale.',
+	'EXTENSION_UPLOAD'					=> 'Laadi laiendus üles',
+	'EXTENSION_UPLOAD_EXPLAIN'			=> 'Siin saad üles laadida kokku pakitud .zip faile. “Laadi laiendus üles” pakib selle ise lahti, ning seadistab selle sulle paigaldamiseks valmis.<br />Vali fail arvutist või kirjuta veebilehe aadress all olevale väljale.',
 	'EXT_UPLOAD_INIT_FAIL'				=> 'Tekkis viga, kui alustati laienduse üles laadimise protsessi.',
 	'EXT_NOT_WRITABLE'					=> 'ext/ kaust ei ole kirjutatav. See on nõutud, et “Lae üles laiendusi” töötaks korralikult. Palun kohanda antud seadistust, ning proovi uuesti.',
 	'EXT_UPLOAD_ERROR'					=> 'Laiendust ei laetud üles. Palun veendu, et laadisid üles õige laienduse .zip faili ja proovi uuesti.',
 	'NO_UPLOAD_FILE'					=> 'Ühtegi faili pole määratud või tekkis viga üles laadimise protsessi jooksul.',
 	'NOT_AN_EXTENSION'					=> 'Üles laetud .zip fail ei ole phpBB laiend. Faili ei salvestatud serverisse.',
 
-	'EXTENSION_UPLOADED'				=> 'Laiend “%s” on edukalt üles laetud.',
+	'EXTENSION_UPLOADED'				=> 'Laiendus “%s” on edukalt üles laaditud.',
 	'EXTENSIONS_AVAILABLE'				=> 'Saadaval laiendused',
 	'EXTENSION_INVALID_LIST'			=> 'Laienduste nimekiri',
-	'EXTENSION_UPLOADED_ENABLE'			=> 'Luba üles laetud laiendused',
-	'ACP_UPLOAD_EXT_UNPACK'				=> 'Paki lahti laiendus',
+	'EXTENSION_UPLOADED_ENABLE'			=> 'Luba üles laaditud laiendused',
+	'ACP_UPLOAD_EXT_UNPACK'				=> 'Paki laiendus lahti',
 	'ACP_UPLOAD_EXT_CONT'				=> 'Paki sisu: %s',
 
 	'EXTENSION_DELETE'					=> 'Kustuta laiendus',
@@ -43,30 +43,30 @@ $lang = array_merge($lang, array(
 	'EXT_DELETE_SUCCESS'				=> 'Laiendus on edukalt kustutatud.',
 
 	'EXTENSION_ZIP_DELETE'				=> 'Kustuta zip fail',
-	'EXTENSION_ZIP_DELETE_CONFIRM'		=> 'Oled sa kindel, et soovid kustutada .zip faili - “%s”?',
-	'EXT_ZIP_DELETE_SUCCESS'			=> 'Laiendi zip fail on edukalt kustutatud.',
+	'EXTENSION_ZIP_DELETE_CONFIRM'		=> 'Oled sa kindel, et soovid kustutada zip faili - “%s”?',
+	'EXT_ZIP_DELETE_SUCCESS'			=> 'Laienduse zip fail on edukalt kustutatud.',
 
 	'ACP_UPLOAD_EXT_ERROR_DEST'			=> 'Vendor\'it või kausta sihtkohta üles laaditud zip failil pole. Faili ei salvestatud serverisse.',
-	'ACP_UPLOAD_EXT_ERROR_COMP'			=> 'composer.json ei leitud üles laetud .zip failist. Faili ei salvestatud serverisse.',
+	'ACP_UPLOAD_EXT_ERROR_COMP'			=> 'composer.json ei leitud üles laaditud zip failist. Faili ei salvestatud serverisse.',
 	'ACP_UPLOAD_EXT_ERROR_NOT_SAVED'	=> 'Faili ei salvestatud serverisse.',
-	'ACP_UPLOAD_EXT_WRONG_RESTORE'		=> 'An error occurred during the update of an installed extension. Try to update it again.', // to translate
+	'ACP_UPLOAD_EXT_WRONG_RESTORE'		=> 'Olemasoleva laienduse uuendamisel tekkis viga. Proovi palun uuesti uuendada.',
 
 	'UPLOAD_EXTENSIONS_DEVELOPERS'		=> 'Arendajad',
 
 	'SHOW_FILETREE'						=> '<< Näita faili puud >>',
 	'HIDE_FILETREE'						=> '>> Peida faili puu <<',
 
-	'EXT_UPLOAD_SAVE_ZIP'				=> 'Salvesta üles laetud .zip fail',
-	'ZIP_UPLOADED'						=> '.zip arhiiv on üles laetud',
+	'EXT_UPLOAD_SAVE_ZIP'				=> 'Salvesta üles laaditud zip fail',
+	'ZIP_UPLOADED'						=> 'zip arhiiv on üles laetud',
 	'EXT_ENABLE'						=> 'Luba',
-	'EXT_UPLOADED'						=> 'üles laetud',
-	'EXT_UPDATED'						=> 'updated', // to translate
-	'EXT_UPDATED_LATEST_VERSION'		=> 'updated to the latest version', // to translate
-	'EXT_UPLOAD_BACK'					=> '« Tagasi Lae üles laiendusi lehele',
+	'EXT_UPLOADED'						=> 'üles laaditud',
+	'EXT_UPDATED'						=> 'uuendatud',
+	'EXT_UPDATED_LATEST_VERSION'		=> 'uuendatud uusimale versioonile',
+	'EXT_UPLOAD_BACK'					=> '« Tagasi “Laadi laiendus üles” lehele',
 
-	'ACP_UPLOAD_EXT_DIR'				=> 'Extensions\' zip packages storage path', // to translate
-	'ACP_UPLOAD_EXT_DIR_EXPLAIN'		=> 'Path under your phpBB root directory, e.g. <samp>ext</samp>.<br />You can change this path to store zip packages in a special folder (for example, if you want to let users download those files, you can change it to <em>downloads</em>, and if you want to prohibit those downloads, you can change it to the path that is upper by one level than http root of your website (or you can create a folder with the appropriate .htaccess file)).', // to translate
+	'ACP_UPLOAD_EXT_DIR'				=> 'Laienduste zip failide teekond',
+	'ACP_UPLOAD_EXT_DIR_EXPLAIN'		=> 'Teekond sinu phpBB root kausta, näiteks <samp>ext</samp>.<br />Sa saad muuta seda teekonda, kuhu salvestakse zip failid (näiteks, kui soovid lasta liikmetel ja ka külalistel allalaadida neid faile, siis muuda see kaustaks <em>downloads</em>. Kui sa aga soovid keelata allalaadimise, siis saad muuta teekonna üks level üles poole minnes oma serveri kaustas, mis ei oleks http root. (Samuti sa võid luua kausta, kus on asjakohane .htaccess fail sees, mis keelaks leheküljele sisenemise ja allalaadimise)).',
 
-	'ACP_UPLOAD_EXT_UPDATED'			=> 'The installed extension was updated.', // to translate
-	'ACP_UPLOAD_EXT_UPDATED_EXPLAIN'	=> 'You have uploaded a zip file for an already installed extension. That extension <strong>was disabled automatically</strong> to make the update process safer. Now please <strong>check</strong> whether the uploaded files are correct and <strong>enable</strong> the extension if it still should be used on the board.', // to translate
+	'ACP_UPLOAD_EXT_UPDATED'			=> 'Paigaldatud laiendus on uuendatud.',
+	'ACP_UPLOAD_EXT_UPDATED_EXPLAIN'	=> 'Sa oled üles laadinud juba olemas oleva laienduse zip faili. Antud laiendus <strong>keelati automaatselt</strong>, et teha uuendamise protsess ohutumaks. Nüüd palun <strong>kontrolli</strong> kas üles laaditud failid oli õiged, ning <strong>luba</strong> laiendus, kui on see kasutusel foorumis.',
 ));
