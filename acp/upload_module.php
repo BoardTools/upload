a6f3b421223e02ed582def85553fb23b1d277603
Forumhulp.com <info@forumhulp.com>
2014-11-10 12:35:05 +0100

    Update upload_module.php

--- acp/upload_module.php
+++ acp/upload_module.php
@@ -83,12 +83,23 @@ class upload_module
 		}
 		$this->self_update = $upload_extensions_download;
 
-		$string = file_get_contents('http://forumhulp.com/ext/phpbb.json');
-		$json_phpbb = json_decode($string, true);
-		$valid_phpbb_ext = '';
-		foreach($json_phpbb as $ext => $value)
+		$valid_phpbb_ext = $file_contents = '';
+		if (!($file_contents = @file_get_contents('http://forumhulp.com/ext/phpbb.json')))
 		{
-			$valid_phpbb_ext .= '<option value="' . $value['download'] . '">' . $ext . ' version: ' . $value['version'] . '</option>';
+			$this->trigger_error($user->lang('FILE_CONTENT_ERR', 'http://forumhulp.com/ext/phpbb.json'), E_USER_NOTICE);
+		}
+
+		if (($metadata = @json_decode($file_contents, true)) === null)
+		{
+			$this->trigger_error($user->lang('FILE_JSON_DECODE_ERR', 'http://forumhulp.com/ext/phpbb.json'), E_USER_NOTICE);
+		}
+
+		if (sizeof($metadata))
+		{
+			foreach($metadata as $ext => $value)
+			{
+				$valid_phpbb_ext .= '<option value="' . $value['download'] . '">' . $ext . ' version: ' . $value['version'] . '</option>';
+			}
 		}
 
 		$template->assign_vars(array('VALID_PHPBB_EXT'	=> $valid_phpbb_ext));
