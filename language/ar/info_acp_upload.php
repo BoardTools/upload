fb303458d7af1296ec20f729c6e4d54b065384bf
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-08-04 00:27:01 +0300

    Create info_acp_upload.php

---
+++ language/ar/info_acp_upload.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @package Upload Extensions
+* @copyright (c) 2014 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* Translated By : Bassel Taha Alhitary - www.alhitary.net
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_UPLOAD_EXT_TITLE'				=> 'رفع الإضافات',
+	'ACP_UPLOAD_EXT_CONFIG_TITLE'		=> 'رفع الإضافات',
+));
