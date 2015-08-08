f49369848a09cbf8b0e3f65015934a74aad711ac
lavigor <lavigor@users.noreply.github.com>
2015-08-08 21:32:52 +0300

    Add Arabic translation. Close #26.

---
+++ ar/info_acp_upload.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @package Upload Extensions
+* @copyright (c) 2014 - 2015 Igor Lavrov (https://github.com/LavIgor) and John Peskens (http://ForumHulp.com)
+* @translated by Basil Taha Alhitary - www.alhitary.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_UPLOAD_EXT_TITLE'				=> 'رفع الإضافات',
+	'ACP_UPLOAD_EXT_CONFIG_TITLE'		=> 'رفع الإضافات',
+));
