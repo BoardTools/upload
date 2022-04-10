3e917ee18b6fd0e70bb80d58d3ce9ce85efe1b86
max-ima <96241406+max-ima@users.noreply.github.com>
2022-04-10 12:38:40 +0200

    Slovenian
    Slovenian translation

---
+++ language/sl/info_acp_upload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* @package Upload Extensions
+* @copyright (c) 2014 - 2019 Igor Lavrov (https://github.com/LavIgor) and John Peskens (http://ForumHulp.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Slovenian Translation - Marko K.(max, max-ima,...)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_UPLOAD_EXT_TITLE'				=> 'Nalaganje razširitev',
+	'ACP_UPLOAD_EXT_CONFIG_TITLE'		=> 'Nalaganje razširitev/<br>Upload Extensions',
+));
